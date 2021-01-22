4520dbb3932f6b86ee0105190d609dd5b27d2bcf
Teten Nugraha <teten.nugraha@allianz.co.id>
2021-01-22 11:31:45 +0700

    database config

---
+++ db/db_config.go
@@ -0,0 +1,41 @@
+package db
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+	"os"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func InitDB() *gorm.DB {
+
+	getENV()
+
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_DATABASE")
+
+	db, err := gorm.Open("mysql", dbUsername+":"+dbPassword+"@tcp("+dbHost+":"+dbPort+")/"+dbName+"?parseTime=true")
+	if err != nil {
+		panic(err)
+	}
+
+	//migrateDDL(db)
+
+	return db
+}
+
+//func migrateDDL(db *gorm.DB) {
+//	db.AutoMigrate(&domain.User{})
+//}
+
+func getENV() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		logrus.Error("Error loading .env file")
+	}
+	logrus.Info("Twitter API succees db connection")
+}
