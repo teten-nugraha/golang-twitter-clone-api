55b6c0246f3d9935de3d683f52474f76d7d8f9e7
Teten Nugraha <teten.nugraha@allianz.co.id>
2021-01-25 10:37:23 +0700

    allow CORS

--- routes/route.go
+++ routes/route.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/teten-nugraha/golang-twitter-clone-api/db"
 	"github.com/teten-nugraha/golang-twitter-clone-api/injection"
 	"net/http"
@@ -18,6 +19,17 @@ func Init() *echo.Echo {
 	routes.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
+	// Gzip Compression
+	routes.Use(middleware.GzipWithConfig(middleware.GzipConfig{
+		Level: 5,
+	}))
+
+	// Allow CORS
+	routes.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
+	}))
 
 	AuthRoute(routes, authApi)
 	UserRoute(routes, userApi)
